88c188767c5a03773a8eb6af721d7e7129578c13
fahimg23 <fahimghani96@gmail.com>
2018-02-09 18:55:23 -0500

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -2,26 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02/09/2018 03:15:50 PM
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:11:46 02/09/2018 
+-- Create Date:    18:25:50 02/09/2018 
 -- Design Name: 
 -- Module Name:    ALU - Behavioral 
 -- Project Name: 
@@ -38,9 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.all;
 use IEEE.numeric_std.all;
---use IEEE.std_logic_arith.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -71,76 +50,81 @@ signal Aunsigned: std_logic_vector(31 downto 0);
 signal Bunsigned: std_logic_vector(31 downto 0);
 signal Asigned: std_logic_vector(31 downto 0);
 signal Bsigned: std_logic_vector(31 downto 0);
-signal out: std_logic_vector(31 downto 0);
+signal output_sig: std_logic_vector(31 downto 0);
+signal Asigned_tmp, Bsigned_tmp :std_logic_vector(31 downto 0);
 begin
 Aunsigned<=inputA;
 Bunsigned<=inputB;
 
 case (opcode) is
-	when "000000" => 
+	when "000000" => -- arithmetic operations (opcode = 00)
 		case(funct) is
-			when "0000" => 
-			    out <=Asigned + Bsigned;
-			    
+			when "0000" =>  --(signed addition)			
+			    if (Asigned(31) != Bsigned(31)) then
+					output_sig <=Asigned + Bsigned;
+					overflow <= '0';
+					carryout <= '0';
+				else
+					
 			when "0001" =>
 				initial_Output_unsigned <= Aunsigned + Bunsigned;
-			when "0010" =>
-				out <=Asigned - Bsigned;
+			when "0010" => --(subtraction)
+				output_sig <=Asigned - Bsigned;
 			when "0011" =>
 				initial_Output_unsigned <= Aunsigned - Bunsigned;
 			when "0100" =>
-				out <= inputA AND inputB;
+				output_sig <= inputA AND inputB;
 			when "0101" =>
-				out <= inputA OR inputB;
+				output_sig <= inputA OR inputB;
 			when "0110" =>
-				out <= inputA XOR inputB;
+				output_sig <= inputA XOR 	inputB;
 			when "0111" =>
-				out <= inputA NOR inputB;
+				output_sig <= inputA NOR inputB;
 			when "1010" => 
 				if(Asigned < Bsigned) then
-					out<= "1";
+					output_sig<= "1";
 				else
-					out<= "0";
+					output_sig<= "0";
 			    end if;
 			when "1011"=>
 			    if(Aunsigned < Bunsigned) then
-                    out<= "1";
+                    output_sig<= "1";
                 else
-                    out<= "0";
+                    output_sig<= "0";
                 end if;
          end case;
     when "001000"=>
-        out<=(Asigned + Bsigned);
+        output_sig<=(Asigned + Bsigned);
     when "001001"=>
-       out<=(Aunsigned + Bunsigned);
+       output_sig<=(Aunsigned + Bunsigned);
     when "001010"=>
        if(Aunsigned < Bunsigned) then
-          out<= "1";
+          output_sig<= "1";
        else
-          out<= "0";
+          output_sig<= "0";
        end if;
     when "001011"=>
        if(Aunsigned < Bunsigned) then
-          out<= "1";
+          output_sig<= "1";
        else
-          out<= "0";
+          output_sig<= "0";
        end if;
     when "001100"=>
-         out <= inputA AND inputB;
+         output_sig <= inputA AND inputB;
     when "001101"=>
-         out <= inputA OR inputB;
+         output_sig <= inputA OR inputB;
     when "001110"=>
-         out <= inputA XOR inputB;
+         output_sig <= inputA XOR inputB;
     when "001111"=>
-         out(15 downto 0)<="0000000000000000";
-         out(31 downto 16)<=inputA;
+         output_sig(15 downto 0)<="0000000000000000";
+         output_sig(31 downto 16)<=inputA;
 end case;
 
-process(out)
-    if (out="00000000000000000000000000000000") then zero<="1";
+process(output_sig)
+    if (output_sig="00000000000000000000000000000000") then zero<="1";
     end if;
 end process;
 				
-output<=out;
+output<=output_sig;
 end Behavioral;
 
