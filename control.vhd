51c44530c8d59b5827de1294176246be30145366
lienwyatt <31666811+lienwyatt@users.noreply.github.com>
2018-04-06 18:59:17 -0400

    Update control.vhd

--- control.vhd
+++ control.vhd
@@ -1,4 +1,3 @@
-----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
 -- 
@@ -35,14 +34,16 @@ entity control is
 port(
 clk: in std_logic;
 IR2, IR3, IR4, IR5 : in std_logic_vector(31 downto 0);
-PCsel, Bsel, LoadSel, Rselect, RegWrite: out std_logic
+PCsel, LoadSel, Rselect, RegWrite, Asel: out std_logic;
+Bsel: out std_logic_vector(1 downto 0)
 );
 end control;
 
 architecture Behavioral of control is
 	signal opcode2, opcode3, opcode4, opcode5: std_logic_vector(5 downto 0);
 	signal funct2, funct3, funct4, funct5: std_logic_vector(5 downto 0);
-	signal opResult1, opResult2, functResult, opAndfunct, opOrFunct, storet, stored: std_logic;
+	signal concat3, concat4: std_logic_vector(11 downto 0);
+	signal opResult1, opResult2, functResult, opAndfunct, opOrFunct, storet, stored, bchoose,B, achoose: std_logic;
 
 	begin
 	opcode2 <= IR2(31 downto 26);
@@ -97,13 +98,37 @@ architecture Behavioral of control is
 --			when others =>
 --		end case;
 --	end process;
+concat4<=opcode4&funct4;
+process(IR4, IR3, storet, stored)
+begin
+    if ((IR4(20 downto 16)=IR3(20 downto 16) AND storet='1')OR(IR4(15 downto 11)=IR3(20 downto 16) AND stored='1')) then bchoose<='1';
+    else bchoose<='0';
+    end if;
+    if ((IR4(20 downto 16)=IR3(20 downto 16) AND storet='1')OR(IR4(15 downto 11)=IR3(25 downto 21) AND stored='1')) then achoose<='1';
+        else achoose<='0';
+   end if;
+end process;
+process(B, bchoose)
+begin
+    if (bchoose='1')then Bsel<="11";
+    elsif(B='1') then Bsel<="01";
+    else Bsel<="00";
+    end if;    
+end process;
+Asel<=achoose;
 
+	with concat4 select stored<=
+	'1' when "000000100000" | "000000100001" | "000000100100" | "000000100101" | "000000000000" | "0000000000100" | "000000101010" | "000000101011" | "000000000011" | "000000000010" | "000000000110" | "000000100011" | "000000100110",
+	'0' when others;
 	
+	with opcode4  select storet<=
+	'1' when "01001" | "001100" | "100000" | "100011" | "001101" | "0011010" | "001011" | "001110" |"01000",
+	'1' when others;
 	
-	with opcode3 select Bsel<=
+	with opcode3 select B<=
 	'0' when "001000" | "001001" |"001010" |"001011" | "001100" | "001101" | "001110" | "001111",
 	'1' when others; -- need to add all of the other opcodes
-	
+
 	with opcode5 select LoadSel <=
 	'1' when "001000" | "001001" |"001010" |"001011" | "001100" |"001101" | "001110" | "001111"| "000000",
 	'0' when others; -- Need to add other opcodes
